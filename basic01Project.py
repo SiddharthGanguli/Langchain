d28c507a2507d564fd6ab6245e637aab37822bb0
Siddhartha Ganguli <siddharthaganguli0093@gmail.com>
2025-07-07 07:30:52 +0530

    after completing basic01 final project

---
+++ basic01Project.py
@@ -0,0 +1,24 @@
+## User friendly chat bot
+
+
+from langchain_community.llms import Ollama
+from langchain_core.messages import HumanMessage
+
+# initilize the model 
+
+model=Ollama(model="gemma:2b")
+
+# users questions
+def users(message):
+    prompt=[HumanMessage(content=message)]
+    respose=model.invoke(prompt)
+    return respose
+if __name__ == "__main__":
+    print("welcome Rik's Local Chat bot -> ")
+    while True: 
+        user_input=input("You: ")
+        if user_input.lower() in ['exit','bye','quit']:
+            break
+        else:
+            ans=users(user_input)
+            print(f"AI : {ans}")
